4769ceea40ed4b416c3e05ec1024adb20a053a7d
ntdattt <datctag0809@gmail.com>
2024-12-19 22:02:52 +0700

    buoi8

---
+++ Models/ChuDe.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShop.Models
+{
+    public class ChuDe : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
